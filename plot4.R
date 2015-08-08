1acaaf171622f143cc19e5bb1b6a7869512c9593
kamcculloch <kamcculloch@gmail.com>
2015-08-08 15:17:34 -0700

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -18,12 +18,15 @@ png("plot4.png", width=480, height=480)
 
 par(mfrow = c(2, 2)) 
 
-plot(DT, GAP, type="l", xlab="", ylab="Global Active Power", cex=0.2)
-plot(DT, V, type="l", xlab="datetime", ylab="Voltage")
-plot(DT, S1, type="l", ylab="Energy Submetering", xlab="")
+  plot(DT, GAP, type="l", xlab="", ylab="Global Active Power", cex=0.2)
 
-lines(DT, S2, type="l", col="red")
-lines(DT, S3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+  plot(DT, V, type="l", xlab="datetime", ylab="Voltage")
 
-plot(DT, GRP, type="l", xlab="datetime", ylab="Global_reactive_power")
+  plot(DT, S1, type="l", ylab="Energy Submetering", xlab="")
+    lines(DT, S2, type="l", col="red")
+    lines(DT, S3, type="l", col="blue")
+    legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+
+  plot(DT, GRP, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
