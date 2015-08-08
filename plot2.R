8e7c5c9811e92e32cdf7f175df1582ce36cccf28
kamcculloch <kamcculloch@gmail.com>
2015-08-08 15:15:06 -0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,12 +1,14 @@
 ## Retrieve Data
-data2 <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 
-## Create Subset of data for the relevant time frame and 
+## Create Subset of data for the relevant time frame
 SubData2 <- data[data$Date == "1/2/2007"|data$Date == "2/2/2007",]
 
-## Create plot from subset of data
+## Define the columns we're trying to plot
 DT <- strptime(paste(SubData2$Date, SubData2$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 GAP <- as.numeric(SubData2$Global_active_power)
+
+## Create plot from subset of data
 png("plot2.png", width=480, height=480)
 plot(DT, GAP, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
